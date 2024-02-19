7a45c1a34bb9761967d0ec16001957fd195601ee
Sebastián Torres <sebastorres325@gmail.com>
2024-02-18 19:32:08 -0500

    Cambios discord

--- co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/EjemploSRP/CalculadoraCambioAceite.java
+++ co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/EjemploSRP/CalculadoraCambioAceite.java
@@ -1,7 +1,7 @@
 package co.edu.uniquindio.solid.EjemploSRP;
 
 // Clase separada para la responsabilidad de calcular el kilometraje
-public class CalculadoraKilometraje {
+public class CalculadoraCambioAceite {
     public void calcularKilometraje(Vehiculo vehiculo) {
         // Código para calcular el kilometraje del vehículo
     }
--- co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/EjemploSRP/Vehiculo.java
+++ co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/EjemploSRP/Vehiculo.java
@@ -4,13 +4,41 @@ package co.edu.uniquindio.solid.EjemploSRP;
 public class Vehiculo {
     private String marca;
     private String modelo;
+    private double kilometraje;
 
-    public Vehiculo(String marca, String modelo) {
+    public Vehiculo(String marca, String modelo, double kilometraje) {
         this.marca = marca;
+        this.modelo = modelo;
+        this.kilometraje = kilometraje;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    public String obtenerDetallesDelVehiculo() {
-        return "Marca: " + marca + ", Modelo: " + modelo;
+    public double getKilometraje() {
+        return kilometraje;
     }
+
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String obtenerDetallesDelVehiculo() {
+        return "Marca: " + marca + ", Modelo: " + modelo + ",kilometraje";
+    }
+
+
 }
